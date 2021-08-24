3c08e76b572f2727eab2445ac1c33f0443d5b2ab
Marcos Trejo <marcostrejo23@gmail.com>
2021-08-23 17:38:19 -0700

    completed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,6 +9,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
 ];
+
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
@@ -22,18 +23,27 @@ self.addEventListener("install", function(evt) {
     self.skipWaiting()
 });
 
-self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList =>{
-            return Promise.all(
-                keyList.map(key =>{
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    )
-    self.clients.claim();
-});
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
+    evt.respondWith(
+      caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+      })
+    );
+  });
